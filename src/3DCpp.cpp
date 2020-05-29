cdd03af6828f89d11d3a0fb5d0f9f655ec7fa3dd
Kevin Rivera <kevinrev26@gmail.com>
2020-05-29 11:41:43 -0600

    event object added to prevent crashes (?)

--- src/3DCpp.cpp
+++ src/3DCpp.cpp
@@ -10,18 +10,34 @@ sf::RenderWindow window(vm, "Hello World", sf::Style::Default);
 // Textures and sprites objects
 sf::Texture skyTexture;
 sf::Sprite skySprite;
+sf::Texture bgTexture;
+sf::Sprite bgSprite;
+sf::Texture heroTexture;
+sf::Sprite heroSprite;
+
 
 // Initialization function
 void init() {
-    // Load Sky Texture
+    // Load Textures
     skyTexture.loadFromFile("Assets/graphics/sky.png");
-    // Attach Texture
+    bgTexture.loadFromFile("Assets/graphics/bg.png");
+    heroTexture.loadFromFile("Assets/graphics/hero.png");
+    // Attach Textures
     skySprite.setTexture(skyTexture);
+    bgSprite.setTexture(bgTexture);
+    heroSprite.setTexture(heroTexture);
+
+    // Positioning
+    heroSprite.setPosition(sf::Vector2f(viewSize.x/2, viewSize.y/2));
+    heroSprite.setOrigin(heroTexture.getSize().x/2,
+                        heroTexture.getSize().y/2);
 }
 
 // Drawing function
 void draw() {
     window.draw(skySprite);
+    window.draw(bgSprite);
+    window.draw(heroSprite);
 }
 
 int main(int argc, char **argv) {
@@ -29,9 +45,12 @@ int main(int argc, char **argv) {
     std::cout << "Starting rendering!\n";
     // Initialize game objects
     init();
-    sf::Vector2f origin = skySprite.getOrigin();
-    std::cout << origin.x;
     while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+            window.close();
+        }
         // Handle Keyboard Events
         // Update Game Objects
         window.clear(sf::Color::Red);
