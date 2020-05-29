69134bb11364167e477fd4c4d1ab5564394df1f5
Kevin Rivera <kevinrev26@gmail.com>
2020-05-29 04:18:11 -0600

    Loading textures from file, assets directory added

--- src/3DCpp.cpp
+++ src/3DCpp.cpp
@@ -1,32 +1,42 @@
 // Copyright 2020 - Kevin Rivera @kevinrev26
-
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
 // Size of the window
 sf::Vector2f viewSize(1024, 768);
 // Setting the size of the window
 sf::VideoMode vm(viewSize.x, viewSize.y);
 // Define a default view window
 sf::RenderWindow window(vm, "Hello World", sf::Style::Default);
+// Textures and sprites objects
+sf::Texture skyTexture;
+sf::Sprite skySprite;
 
+// Initialization function
+void init() {
+    // Load Sky Texture
+    skyTexture.loadFromFile("Assets/graphics/sky.png");
+    // Attach Texture
+    skySprite.setTexture(skyTexture);
+}
+
+// Drawing function
+void draw() {
+    window.draw(skySprite);
+}
 
 int main(int argc, char **argv) {
-    // Setting a rectangle
-    sf::RectangleShape rectangle(sf::Vector2f(500.0f, 300.0f));
-    rectangle.setFillColor(sf::Color::Magenta);
-    rectangle.setPosition(viewSize.x/2, viewSize.y/2);
-    rectangle.setOrigin(sf::Vector2f(
-        rectangle.getSize().x/2,
-        rectangle.getSize().y/2));
+    //
     std::cout << "Starting rendering!\n";
     // Initialize game objects
+    init();
+    sf::Vector2f origin = skySprite.getOrigin();
+    std::cout << origin.x;
     while (window.isOpen()) {
         // Handle Keyboard Events
         // Update Game Objects
         window.clear(sf::Color::Red);
         // Render Game Objects
-        window.draw(rectangle);
+        draw();
         window.display();
     }
 
